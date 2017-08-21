2b5edbd58d049f7b5449de5e615b5b7f588dccb6
jeanpm <jean.martins@gmail.com>
2017-08-21 13:33:23 -0300

    Atualização Seg Ago 21 13:33:22 BRT 2017

--- AE22CP-172/fundamentos/21-Agosto/observar-vazamento.c
+++ AE22CP-172/fundamentos/21-Agosto/observar-vazamento.c
@@ -4,10 +4,19 @@
 #include <string.h>
 #include <assert.h>
 
+char* at(char* c, int i) {
+    return &c[i];
+}
+
+void print(char* c, int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", c[i]);
+    printf("\n");
+}
+
 int main() {    
-    char* ptr[10];
-    for (int i = 0; i < 10; i++)
-        ptr[i] = malloc(sizeof(char) * 1000);  
-    free(ptr);    
+    char* ptr = malloc(sizeof(char) * 10);  
+    *at(ptr, 5) = 12;
+    print(ptr, 10);
     return 0;
 }
