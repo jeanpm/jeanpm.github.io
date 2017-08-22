3ef47e400a2366539bc6c459ab91f50fa88c50b2
jeanpm <jean.martins@gmail.com>
2017-08-22 13:05:21 -0300

    Atualização Ter Ago 22 13:05:21 BRT 2017

--- AE22CP-172/fundamentos/21-Agosto/vazamento-mem.c
+++ AE22CP-172/fundamentos/21-Agosto/vazamento-mem.c
@@ -1,11 +1,16 @@
 
 #include <stdlib.h>
 
-char* at(char* c, int i) {
+typedef char Type;
+
+Type* at(Type* c, int i) {
     return &c[i];
 }
+
+
+
 int main() {    
-    char* ptr = malloc(sizeof(char) * 10);  
+    Type* ptr = malloc(sizeof(Type) * 10);  
     *at(ptr, 5) = 'j';
     
     return 0;
