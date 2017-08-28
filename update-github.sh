d75a6660b02d6be975145fa889dc92a11aeb7b8e
jeanpm <jean.martins@gmail.com>
2017-08-28 10:41:08 -0300

    Atualização Seg Ago 28 10:41:08 BRT 2017

--- update-github.sh
+++ update-github.sh
@@ -1,6 +1,19 @@
-rsync -r ~/UTFPR-web/_site/* .
+## Diretório onde os arquivos do jekyll estão
+web="~/UTFPR-web"
+
+
+## Copia arquivos gerados pelo jekyll para este diretório
+rsync -r $web/_site/* .
+
+
+## Copia os arquivos de markdown para backup no github
+rsync -r $web/AE22CP-172/* ./AE22CP-172-md-files/
+rsync -r $web/AE22CP-171/* ./AE22CP-171-md-files/
+rsync -r $web/LC21CP-172/* ./LC21CP-172-md-files/
+
 
 git add .
 git commit -m "Atualização `date`"
+
 
 git push -u origin master
